65e716fd5934faac97b1d1c1b5e2ea535be470b0
대현 조 <ianjk553@gmail.com>
2022-03-18 13:51:57 +0900

    이온 어태커 위성 미사일 알고리즘 구성

--- Fortress/Object/Missile/Satellite_Missile.cpp
+++ Fortress/Object/Missile/Satellite_Missile.cpp
@@ -3,13 +3,16 @@
 
 
 Satellite_Missile::Satellite_Missile(std::string const& name, _float2 const& pos, unsigned const width, unsigned const height, _float2 const& missile_range, int const damage, Effect::Type const effect_type)
-	:Missile(name, pos, width, height, missile_range, damage, effect_type, Missile::Angle_Type::Angle), satellite_state{ State::Satellite }
+	:Missile(name, pos, width, height, missile_range, damage, effect_type, Missile::Angle_Type::Angle),
+	satellite_state{ Satellite_State::Initialize }, target_point{0,0}
 {
 }
 
 Satellite_Missile::~Satellite_Missile()
 {
 }
+
+
 
 void Satellite_Missile::check_state()
 {
@@ -19,21 +22,10 @@ void Satellite_Missile::check_state()
 	{
 		break;
 	}
-	//TODO: 위성소환을 여기서 할지 방정식 함수에서 이동계산을 할지 고민/결정 해야함
-	// 이동하는동안 별도 애니메이션 재생, 정지 후 위성 작동 애니메이션 내보냄 
-	//레이저는 이펙트로 처리, 레이저 종료후 폭발 이펙트 생성 및 delete로 넘어감
+
 	case Missile::State::Collide:
 	{	
-		switch (satellite_state)
-		{
-		case Satellite_Missile::State::Satellite:
-		{
-			break;
-		}
-		case Satellite_Missile::State::Ion:
-			break;
 
-		}
 		hit_count++;
 		boom(_Map_manager->hmapdc);  //맵파괴
 		_Physics_manager->collide_bomb(*this, _Tank->tanks);
@@ -58,4 +50,57 @@ void Satellite_Missile::check_state()
 
 	}
 
+}
+
+void Satellite_Missile::ballistics_equation(float const delta, float const wind)
+{
+	if (this->state == Missile::State::Collide)
+	{
+
+		switch (satellite_state)
+		{
+		case Satellite_Missile::Satellite_State::Initialize:
+		{
+			target_point = this->pos;
+			this->pos.y = 0;
+			satellite_state = Satellite_State::Move;
+			this->name = "Ion_attacker/satellite_move";
+			break;
+		}
+		case Satellite_Missile::Satellite_State::Move:
+		{
+			this->pos.y += 500 * delta;
+			if (this->pos.y > 100.0f)
+			{
+				satellite_state = Satellite_State::Active;
+				this->name = "Ion_attacker/satellite_active";
+			}
+			break;
+		}
+		case Satellite_Missile::Satellite_State::Active:
+		{
+			break;
+		}
+		case Satellite_Missile::Satellite_State::Launch:
+		{
+			break;
+		}
+	}
+
+	_float2 const previous = { pos };
+	if (falling)
+	{
+		velocity0.x += wind * delta;
+		this->moving_time += delta * speed;
+		this->pos.x = this->pos0.x + (velocity0.x) * moving_time;
+		this->pos.y = this->pos0.y - velocity0.y * moving_time
+			+ (grav_accerl * static_cast<float>(pow(moving_time, 2))) / 2;
+	}
+	if (angle_type == Angle_Type::Angle)
+		animation.Angle = -atan2(pos.y - previous.y, pos.x - previous.x) * 180 / PI;
+
+	if (this->pos.y > MAPSIZE_H + OUT_RANGE or this->pos.x > MAPSIZE_W + OUT_RANGE or this->pos.x < 0 - OUT_RANGE - MAPSIZE_W)
+	{
+		this->out = true;
+	}
 }
--- Fortress/Object/Missile/Satellite_Missile.h
+++ Fortress/Object/Missile/Satellite_Missile.h
@@ -1,18 +1,46 @@
 #pragma once
 class Satellite_Missile : public Missile
 {
-	enum class State
+	enum class Satellite_State
 	{
-		Satellite, Ion
+		Initialize, Move, Active, Launch
 	};
 private:
-	State satellite_state;
+	Satellite_State satellite_state;
 	void check_state()override;
+	_float2 target_point;
+	float const satellite_speed = 500;
 public:
 	Satellite_Missile(std::string const& name, _float2 const& pos, unsigned const width, unsigned const height,
 		_float2 const& missile_range, int const damage, Effect::Type const effect_type);
 	~Satellite_Missile()override;
+	void ballistics_equation(float const delta, float const wind = 0)override;
 
 };
 //충돌지점 y좌표 조정 위성소환
 //위성 애니메이션
+
+// 위성소환을 check_state에서 할지 방정식 함수에서 케이스로 나누어 이동계산을 할지
+//  고민/결정 해야함
+// 이동하는동안 별도 위성_이동 애니메이션 재생, 정지 후 위성_작동 애니메이션 내보냄 
+//레이저는 이펙트로 처리, 레이저 종료후 폭발 이펙트 생성 및 delete로 넘어감
+// 
+// 알고리즘 정리
+// Initialize
+// 목표 좌표 저장, 위성 위치 설정 및 애니메이션 설정
+// 
+// 미사일 충돌시
+// ballistics에서 미사일의 pos를 타겟 포인트로 저장
+// 이동 상태
+// 렌더 애니메이션은 미사일에서 위성_이동 애니메이션으로 변환
+// y좌표를 최상단에서 델타식으로 아래 100만큼 하방 이동
+// 이동완료 후 Move에서 Active로 전환
+// 
+// 작동 상태
+// 렌더 애니메이션은 위성_작동 애니메이션으로 변환
+// active 애니메이션 재생, 활성화 시간 경과 후 재생 중간시간에 Effect 위성 빔 생성
+// Launch로 전환
+// 
+// Launch
+// effect 사라진 후(혹은 active 애니메이션이 끝난 후 ) 지형 파괴 및 데미지 부여
+//
